60e98d7ed2da5c6b1815caf3a915df6ccaddbc1a
Hritesh1728 <hriteshalw1728@gmail.com>
2025-03-14 21:01:30 +0530

    Render service

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ import cors from 'cors';
 import path from 'path';
 const app = express();
 const PORT = process.env.PORT || 3000;
+import { fileURLToPath } from 'url';
+
+// Convert `__dirname` to work with ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(cors()); // Enable CORS for all routes
 app.use(express.json());
